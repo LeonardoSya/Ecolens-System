d2824cc7b826be4c6208b66714f3e35c0dcdbf87
LeonardoSya <2246866774@qq.com>
2023-12-31 15:14:03 +0800

    modified:   src/pages/services/overview/honeycomb-heat-map.tsx

--- src/pages/services/overview/honeycomb-heat-map.tsx
+++ src/pages/services/overview/honeycomb-heat-map.tsx
@@ -1,11 +1,3 @@
-/*
- * @Author: LeonardoSya 2246866774@qq.com
- * @Date: 2023-12-31 13:34:51
- * @LastEditors: LeonardoSya 2246866774@qq.com
- * @LastEditTime: 2023-12-31 15:11:14
- * @FilePath: \vite-project\src\pages\services\overview\honeycomb-heat-map.tsx
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 import React, { useState, useEffect } from 'react';
 import { HexbinMap } from '@ant-design/maps';
 
